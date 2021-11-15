13f55437ac151699f646057820e4a26f971ec1d4
Handrys <handry.hard228@gmail.com>
2021-11-15 19:44:54 +0200

    Полностью реализовал таймер

--- #source/js/script.js
+++ #source/js/script.js
@@ -22,57 +22,109 @@ if (iconMenu) {
 
 
 //==================== Настройки таймера ======================
-
-let minutesOptions = '2';  //Всего минут
+let currentRound = 'pomodoro';  // {pomodoro, smallBreak, bigBreak}
+let pomMinutesOpt = '25';  //Длительность помидора (в мин)
 //let secondsOptions; 
-let smallBreakOptions; //Маленький перерыв (в сек)
-let bigBreakOptions; //Большой перерыв (в сек)
-let roundsOptions; //Кол-во раундов
-let soundOptions; //True/False
+let smallBreakOpt = '1'; //Маленький перерыв (в мин)
+let bigBreakOpt = '15'; //Большой перерыв (в мин)
+let roundsOpt = 5; //Кол-во раундов до большого перерыва (5)
+let soundOpt; //True/False
 
+let totalRounds = 0; //Пройдено раундов (в начале игры - 0)
+
+let bckgRed = 'linear-gradient(45deg, rgba(114, 3, 3, 0.822) 2%, rgb(199, 49, 35) 90%)';
+let bckgBlue = 'linear-gradient(45deg, rgba(46, 3, 114, 0.822) 2%, rgb(35, 133, 199) 90%)';
 
 //==================== Таймер ======================
-let minutes = minutesOptions;
+let minutes = pomMinutesOpt;
 let seconds = '0';
 
-function template() {
-	if(minutes < 10 && minutes.toString().split('').length == 1){
-		minutes = '0' + minutes;
+//===== (отрисовка на странице) ======
+function template(value){
+	if(value < 10 && value.toString().split('').length == 1){
+		value = '0' + value;
 	}
 	if(seconds < 10 && seconds.toString().split('').length == 1){
 		seconds = '0' + seconds;
 	}
-	document.querySelector('.timer-data__number-minutes').innerHTML = minutes;
+	document.querySelector('.timer-data__number-minutes').innerHTML = value;
 	document.querySelector('.timer-data__number-seconds').innerHTML = seconds;
 }
 
-function start(){
-	minutes = minutesOptions -1;
-	seconds = '59';
-
-	template();
-
+//====== Сам таймер (запускается на кнопку "Старт") ======
+function start(options){
+	minutes = options - 1;
+	seconds = '60';
+	console.log(minutes)
 	let minutes_interval = setInterval(minutesTimer, 60000);
 	let seconds_interval = setInterval(secondsTimer, 1000);
 
 	function minutesTimer(){
+		template(minutes);
 		minutes = minutes -1;
-		template();
 	}
 	function secondsTimer(){
 		seconds = seconds -1;
-		template();
+		template(minutes);
 		if (seconds <= 0) {
 			if (minutes <= 0){
 				clearInterval(minutes_interval);
-				clearInterval(seconds_interval)
+				clearInterval(seconds_interval);
+				checkRound();
 			}
 			seconds = 60;
 		}
 	}
+
+	document.querySelector('.timer-header__skip').addEventListener('click', () => {
+		clearInterval(minutes_interval);
+		clearInterval(seconds_interval);
+	});
 }
 
-template();
 
-document.querySelector('.timer-start').addEventListener('click',start);
-//==========================================================================
+//===== Смена раундов (помидор, перерыв) =====
+function checkRound(){
+	if (totalRounds < roundsOpt +3){
+		if (currentRound == 'pomodoro'){
+			currentRound = 'smallBreak';
+			document.querySelector('.content-timer__window').style.background = bckgBlue;
+			document.querySelector('.timer-data__info').innerHTML = '[перерыв]';
+			template(smallBreakOpt);
+		} else{ 
+			currentRound = 'pomodoro';
+			document.querySelector('.content-timer__window').style.background = bckgRed;
+			document.querySelector('.timer-data__info').innerHTML = '[помидорка]';
+			template(pomMinutesOpt);
+		}	
+		totalRounds++;
+	} else {
+		currentRound = 'bigBreak';
+		document.querySelector('.content-timer__window').style.background = bckgBlue;
+		document.querySelector('.timer-data__info').innerHTML = '[большой перерыв]';
+		totalRounds = 0;
+		template(bigBreakOpt);
+	}
+}
+
+template(minutes);
+
+
+//========= События на кнопки ==============
+
+document.querySelector('.timer-start').addEventListener('click', () => {
+	if (currentRound == 'pomodoro') start(pomMinutesOpt);
+	if (currentRound == 'smallBreak') start(smallBreakOpt);
+	if (currentRound == 'bigBreak') start(bigBreakOpt);
+
+});
+
+document.querySelector('.timer-stop').addEventListener('click', () => {
+	window.location.reload();
+});
+
+document.querySelector('.timer-header__skip').addEventListener('click', () => {
+	seconds = 0;
+	checkRound();
+});
+//=============================================================================================================
